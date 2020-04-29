28c1949efc4ec7c1b9faeb65290c67543f3c816f
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-04-29 13:47:07 +0530

    if else demo

---
+++ IFDEMO.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	clrscr();
+	printf("\n enter value ");
+	scanf("%d",&a);
+	if(a==5)
+	{
+		printf("\n you enter 5 ");
+	}
+	else
+	{
+		printf("\n you not enter 5");
+	}
+	getch();
+}
