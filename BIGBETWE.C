bcb25a35c50f8a1a3f222966033c2bac77f9f7fd
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-04-29 13:33:58 +0530

    big between 3 numbers

---
+++ BIGBETWE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1,n2;
+	clrscr();
+	printf("\n enter 1 number ");
+	scanf("%d",&n1);
+	printf("\n enter 2 number ");
+	scanf("%d",&n2);
+	if(n1>n2)
+	{
+		printf("\n n1 is big ");
+	}
+	else
+	{
+		printf("\n n2 is big ");
+	}
+	getch();
+}
