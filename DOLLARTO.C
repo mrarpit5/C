1150cedce34275f08f04e4e7e0d2f62817d0e5e3
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-04-29 13:41:28 +0530

    convert dollar to rupees

---
+++ DOLLARTO.C
@@ -0,0 +1,13 @@
+//1dollar=50rupees
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int dollar,rupees;
+	clrscr();
+	printf("\n enter dollar");
+	scanf("%d",&dollar);
+	rupees=dollar*50;
+	printf("\n rupees is %d ",rupees);
+	getch();
+}
