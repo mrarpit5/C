3827c4c8af7334521934092fdef8162956dd7158
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-04-29 13:50:23 +0530

    check number is odd or even

---
+++ NOISODDO.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	clrscr();
+	printf("\n enter mumber ");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("\n number is even ");
+	}
+	else
+	{
+		printf("\n number is odd ");
+	}
+	getch();
+}
