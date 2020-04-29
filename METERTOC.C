88aed084675c7b46d8fe2fc040a9e26c4f4f3cfb
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-04-29 13:49:06 +0530

    convert metervto centimeter

---
+++ METERTOC.C
@@ -0,0 +1,13 @@
+//1m=100cm
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int meter,centimeter;
+	clrscr();
+	printf("\n enter meter");
+	scanf("%d",&meter);
+	centimeter=meter*100;
+	printf("\n centimeter is %d ",centimeter);
+	getch();
+}
