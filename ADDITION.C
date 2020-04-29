dc5c9b3591d84f4d2a9b41a280ce0e7fc41370f2
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-04-29 13:29:03 +0530

    addtion of two numbers

---
+++ ADDITION.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("enter a");
+	scanf("%d",&a);
+	printf("enter b");
+	scanf("%d",&b);
+	c=a+b;
+	printf("ans is %d ",c);
+	getch();
+}
