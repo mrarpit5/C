4385ce935b4076d70cbd921aaaa8b9b5673ad2b6
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-04-29 13:38:29 +0530

    convert celsius to fahrenheit

---
+++ CELSIUST.C
@@ -0,0 +1,13 @@
+//c=33,f=99
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int celsius,fahrenheit;
+	clrscr();
+	printf("\n enter celsius");
+	scanf("%d",&celsius);
+	fahrenheit=celsius*3;
+	printf("\n fahrenheit is %d ",fahrenheit);
+	getch();
+}
