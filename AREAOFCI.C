5f142705c1483d69ac7a395a2a82005d870e6ad0
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-04-29 13:30:12 +0530

    programe that perform area of circle

---
+++ AREAOFCI.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int r;
+	float pi=3.14,ac;
+	clrscr();
+	printf("\n enter value of r");
+	scanf("%d",&r);
+	ac=pi*r*r;
+	printf("\n area of circle is %f ",ac);
+	getch();
+}
